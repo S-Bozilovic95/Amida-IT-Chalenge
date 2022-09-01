198c5c9c0e09fdf7e364d6ca9dafb34415333c47
Lacedaimon95 <svetozar95@gmail.com>
2022-09-01 03:27:02 +0200

    master/add functionalities

--- rock-paper-scissors/src/components/PlayGround.tsx
+++ rock-paper-scissors/src/components/PlayGround.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { act } from 'react-dom/test-utils';
 
 type PlayGroundProps = {
     
@@ -7,21 +8,46 @@ type PlayGroundProps = {
 export const PlayGround: React.FC<PlayGroundProps> = ({}) => {
     const[player,setPlayer]= useState<string>("");
     const[pcPlay,setPcPlay]=useState<string>("");
+    let[num,setnum]= useState<number>(0)
 
     const handlePlayerChoice = (e: any)=>{
         setPlayer(e.target.value);   
     }
 
-    const handleRound =()=>{
+    
+    const handleRound = ()=>{
         let arr =["papper","scissors","rock"];
-        let rand =Math.floor(Math.random()*arr.length);
-        console.log(arr[rand]);
-        setPcPlay(arr[rand])
+
+           if(num===0){
+            let roll = setInterval(()=>{
+                let rand =Math.floor(Math.random()*arr.length);
+                setPcPlay(arr[rand])
+                // // console.log(active);
+                // console.log("radi");
+                setnum(num++)
+
+                
+                if(num>4){
+                    clearInterval(roll);
+                    // console.log("stani");
+                    setnum(0);
+                }
+                
+            },1000) 
+           }
+       
     }
+
+
+    
+console.log(num);
+
+    
 
 
     return (
         <>
+        {num}
             <h2>{pcPlay}</h2>
             <h2>{player}</h2>
             <div>
@@ -29,7 +55,7 @@ export const PlayGround: React.FC<PlayGroundProps> = ({}) => {
                 <button value={"scissors"} onClick={(e)=>handlePlayerChoice(e)}>scissors</button>
                 <button value={"rock"} onClick={(e)=>handlePlayerChoice(e)}>rock</button>
             </div>
-            <button onClick={()=>handleRound()}>round</button>
+            <button onClick={()=>handleRound()}>start round</button>
         </>
     );
 }
